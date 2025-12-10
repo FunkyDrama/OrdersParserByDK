a0fa226f1f8775a5e7b74bc11640aef1971a0b03
Daniel Kravchenko <cripswhigga18@gmail.com>
2025-12-10 19:56:12 +0200

    - added command to build executable for Windows.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# PyInstaller command to create a standalone executable for macOS/Linux:
+
 # pyinstaller --onefile --windowed \
 # --add-data="config/token.json:config" \
 # --add-data="config/.env:config" \
@@ -8,6 +10,11 @@
 # --hidden-import="pydantic" \
 # --name=OrdersParserByDK \
 # main.py
+
+# Alternative command for Windows:
+
+# pyinstaller --onefile --add-data "config/token.json;config" --add-data "config/.env;config" --add-data "google_api;google_api" --add-data "marketplaces;marketplaces" --icon=assets/icon.png --hidden-import "pydantic_settings" --hidden-import "pydantic" --name OrdersParserByDK main.py
+
 
 import os
 import sys
